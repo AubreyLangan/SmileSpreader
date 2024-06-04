6322464ddd172d1bdd875e8e43ede8e6a244f84a
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-04 13:49:37 -0700

    Commit 4 June 2024

---
+++ Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <img></img>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
