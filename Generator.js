2980dfca416a5602ae838f936990664491ec87cc
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-05 13:58:22 -0700

    Commit 5 June 2024

---
+++ Generator.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const RandomActofKindness = () => {
+    const actsOfKindness = [
+        "Smile at a stranger.",
+        "Hold the door open for someone.",
+        "Pay for the person behind you in line.",
+        "Write a thank you note to someone who has helped you.",
+        "Give someone a compliment.",
+        "Donate unused items to a local charity.",
+        "Send a positive message to a friend or family member.",
+        "Help someone with their groceries.",
+        "Leave a kind note in a public place for someone to find.",
+        "Volunteer your time to help others in need.",
+    ];
