1ecfd4affac7fef3ca477f1e4663ff91a8c6b94f
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-10 13:19:01 -0700

    Commit 10 June 2024

--- Header.js
+++ Header.js
@@ -1,9 +1,11 @@
 import React from "react";
-import 'App.css';
+import '../App.js';
+import './Header.css';
 
 function Header() {
     return (
         <header className="header">
+        <img src="/pictures/Smile Spreaders Banner.png" alt="logo"/>
             <nav>
                 <ul>
                     <li><a href="/">Home</a></li>
