1664d11cae1973c7ab55e714b84e734ed27e4027
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-06-11 09:08:27 -0700

    Commit 11 June 2024

--- App.js
+++ App.js
@@ -5,15 +5,19 @@ import CustomNavbar from "./components/CustomNavbar"
 import Header from "./components/Header";
 import RandomActofKindness from "./components/Generator";
 import Tracker from "./components/Tracker";
+import Chat from "./components/Chat";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <CustomNavbar />
-            <h1>Welcome to Smile Spreaders</h1>
-            <RandomActofKindness />
-            <Tracker />
+            <div className="content">
+                <h1>Welcome to Smile Spreaders</h1>
+                <RandomActofKindness />
+                <Tracker />
+                <Chat />
+            </div>
         </div>
     );
 }
